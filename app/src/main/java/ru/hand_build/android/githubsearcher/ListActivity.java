897b88f4a3ddcd3b9a59eda6442bb395fcc3a6cf
feriatos <vov8278@yandex.ru>
2015-06-15 17:24:15 +0300

    list view added

---
+++ app/src/main/java/ru/hand_build/android/githubsearcher/ListActivity.java
@@ -0,0 +1,33 @@
+package ru.hand_build.android.githubsearcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class ListActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = {"string1", "string2", "string3", "string4", "string5"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new GitAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+}
